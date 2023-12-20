2a25e4435befdd72c8e1f72365ec384679f4e242
Sanket Garde <sanketgarde7473@gmail.com>
2023-12-20 11:28:53 +0530

    Create a employee payroll table in the payroll service database

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -5,4 +5,14 @@ show databases;
 use payroll_service;
 
 
+//UC2 create employee payroll table in the payroll service database
 
+create table employee_payroll
+(
+id int unsigned not null auto_increment,
+name varchar(150) not null,
+salary double not null,
+start date not null,
+primary key(id)
+
+);

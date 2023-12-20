0e52ce73d5e4604dba55e3ab200d6c330b2987e4
Sanket Garde <sanketgarde7473@gmail.com>
2023-12-20 11:22:40 +0530

    Create a payroll service database

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,1 +1,8 @@
-Welcome to payroll service 
+Welcome to the payroll service 
+//UC1 create a database, use database, and show all the databases.
+create database payroll_service;
+show databases;
+use payroll_service;
+
+
+
